36ce4acd9b3a6b170d4f8a946f482f0ff956ed09
hardikbh <hardik16122000@gmail.com>
2022-08-11 10:53:24 +0530

    push

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ namespace AppForGithub
             Console.WriteLine("Hello World!");
             Console.WriteLine("hello");
             Console.WriteLine("hello my world");
+            Console.WriteLine("new project");
+
         }
     }
 }
